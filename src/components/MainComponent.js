e15b425226837195e600d3f1868f95cc31a20ba5
loquence <ryandryv@gmail.com>
2019-01-02 22:02:47 -0500

    SPA part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -26,6 +26,13 @@ class Main extends Component {
     
     
   render() {
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    
     const HomePage = () => {
       return(
         <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
@@ -40,6 +47,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
